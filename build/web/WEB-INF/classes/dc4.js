7300df19cc6c4f5774043c627f7f91b05d877fcc
MarianaSequeira <mfs98@live.com.pt>
2020-10-15 16:27:48 +0100

    Removing old stuff

--- build/web/WEB-INF/classes/dc4.js
+++
@@ -1,9 +0,0 @@
-{
-    "config": {
-        "name": "Diseasecard",
-        "description": "DiseaseCard v4.5",
-        "solr":"http://localhost:8983/solr/",
-        "index":"jdbc:mysql://localhost:3306/diseasecard?user=diseasecard&password=diseasecard",
-        "redis": {"host":"localhost", "port":6379}
-    }
-}
--- build/web/final/view/frame.jsp
+++
@@ -1,2 +0,0 @@
-<%-- LiveView content page (url loaded on server). --%>
-<iframe src="${actionBean.url}" id="_content" width="100%" height="100%" />
